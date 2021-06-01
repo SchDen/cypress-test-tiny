ff96f7c58b51748384620f2d512049fbfd93ebbb
Denis <tony@mac.local>
2021-06-02 02:31:25 +0300

    other: good version v4.12.0

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,14 +1,16 @@
 /// <reference types="cypress" />
 context('Intercept', () => {
     it('chekc intercept 1', () => {
-        cy.intercept('/plate.stl').as('files');
+        cy.server();
+        cy.route('/plate.stl').as('files');
         cy.visit('localhost:8010');
 
         cy.wait('@files');
     });
 
     it('chekc intercept 2', () => {
-        cy.intercept('/plate.stl').as('files');
+        cy.server();
+        cy.route('/plate.stl').as('files');
         cy.visit('localhost:8010');
 
         cy.wait('@files');
